486f0addc8d0a815519acf27ae5d496b1d7c6f91
Luanna <Luanna@DESKTOP-A4PE95R>
2026-09-18 10:08:04 -0300

    Criação e configuração dos modelos de Veículo e Consumo

---
+++ mf--apis-web-services-fuel-manager/Models/Consumo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf__apis_web_services_fuel_manager.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+        [Required]
+        public int VeiculoId { get; set; }
+        [Required]
+        public Veiculo Veiculo { get; set; }
+    }
+    public enum TipoCombustivel
+    {
+        Gasolina,
+        Etanol,
+        Diesel,
+    }
+}
+
---
+++ mf--apis-web-services-fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf__apis_web_services_fuel_manager.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {    //Key = chave primaria
+        //required = obrigatorio ter
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int AnoFabricacao { get; set; }
+        [Required]
+        public int AnoModelo { get; set; }
+        [Required]
+        public ICollection<Consumo> Consumos { get; set; }
+    }
+}
+
+
+                        
+
